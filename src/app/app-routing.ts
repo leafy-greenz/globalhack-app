b42106526742b7cf024d84665be722a5e69d4b87
Michael Pieper <michaelepieper@gmail.com>
2018-10-13 10:20:38 -0500

    Update base component and style template

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,4 +1,4 @@
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
