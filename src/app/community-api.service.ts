7855f770d3d303cab713c6dc1c604c482056ca58
Greg Bahr <gregsbahr@gmail.com>
2018-10-13 15:25:51 -0500

    get communities

--- src/app/community-api.service.ts
+++ src/app/community-api.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Community} from './lib/models/Community';
+
+const API_URL = 'localhost:3000';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,11 @@ export class CommunityAPIService {
 
   constructor(private http: HttpClient) { }
 
+  getAllCommunities(): Observable<Community[]> {
+    return this.http.get<Community[]>(`${API_URL}/community`);
+  }
 
+  getCommunityById(id: string): Observable<Community> {
+    return this.http.get<Community>(`${API_URL}/community/${id}`);
+  }
 }
