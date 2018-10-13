b057216f4a6cb8eb939e977c0e5a07dec255fbb6
YasiTL <yasaman.most@gmail.com>
2018-10-13 15:17:40 -0500

    fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CreateCommunityComponent } from './create-community/create-community.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 
-import {MatButtonModule, MatStepperModule} from '@angular/material';
+import {MatButtonModule, MatStepperModule, MatCardModule, MatSidenavModule, MatDividerModule, MatTabsModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -28,13 +28,10 @@ import {MatButtonModule, MatStepperModule} from '@angular/material';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
-=======
     MatCardModule,
     MatSidenavModule,
     MatDividerModule,
     MatTabsModule,
->>>>>>> 15d0d90445cf6bf620d846349e4c1e2cb3fa65a0
     MatButtonModule,
     MatStepperModule
   ],
