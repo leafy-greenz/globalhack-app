f3660eee17b967c8c6c6bd47406f132ba3c33952
Greg Bahr <gregsbahr@gmail.com>
2018-10-13 16:20:46 -0500

    user api calls

--- src/app/community-api.service.ts
+++ src/app/community-api.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Community} from './lib/models/Community';
+import {User} from './lib/models/User';
 
 const API_URL = 'http://localhost:3000';
 
@@ -37,4 +38,28 @@ export class CommunityAPIService {
 
     return this.http.put<Community>(`${API_URL}/community/${id}`, community, { headers });
   }
+
+  createUser(user: User): Observable<User> {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post<User>(`${API_URL}/user`, user, { headers });
+  }
+
+  removeUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${API_URL}/user/${id}`);
+  }
+
+  updateUser(id: string, user: User): Observable<User> {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.put<User>(`${API_URL}/user/${id}`, user, { headers });
+  }
+
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(`${API_URL}/user/${id}`);
+  }
+
+
 }
